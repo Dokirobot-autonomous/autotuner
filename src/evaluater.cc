dd70e1628ce2128bb803ea434633970670a87387
shrobot100 <sh.oh54t@gmail.com>
2020-09-10 16:37:37 +0900

    csv

--- src/evaluater.cc
+++ src/evaluater.cc
@@ -1,40 +1,18 @@
 #include <ros/ros.h>
-
-#include <nav_msgs/Odometry.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
+#include <vector>
 #include <math.h>
 #include <tf/transform_listener.h>
+#include "csv.h"
 
-#define MAX_EVALUATION_DISTANCE 1.0 
+#define MAX_EVALUATION_DISTANCE 3.0 
 
 using namespace geometry_msgs;
-using namespace nav_msgs;
-using namespace message_filters;
 using namespace std_msgs;
-ros::Publisher output_pub;
-ros::Publisher output_division;
 
 void GetRPY(const geometry_msgs::Quaternion &q,
 		double &roll,double &pitch,double &yaw){
 	tf::Quaternion quat(q.x,q.y,q.z,q.w);
 	tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
-}
-//map座標系におけるlidar座標を取得
-void getLidarPose(const PoseStampedConstPtr &in, PoseStamped *out){
-	double r,p,y;
-	const double d = 0.33;
-	Quaternion q = in->pose.orientation;
-	out->header = in->header;
-	out->header.frame_id = "map";
-
-	GetRPY(q,r,p,y);
-
-	out->pose.position.x = in->pose.position.x - d*cos(y)+d;
-	out->pose.position.y = in->pose.position.y - d*sin(y);
-	out->pose.position.z = 0.0;
-	out->pose.orientation = q;
 }
 double calError(tf::Vector3 &a,tf::Vector3 &b){
 	double ret,ex,ey;
@@ -43,14 +21,17 @@ double calError(tf::Vector3 &a,tf::Vector3 &b){
 	ret = ex*ex + ey*ey;
 	return ret;
 }
+/*
 void callback(const OdometryConstPtr &topic1, const PoseStampedConstPtr &topic2){
 	static double distance = 0.0;
+	static int cnt=0;
 	static double error = 0;
 	static bool flag=false;
 	PoseStamped orb_lidar_pos;
-	PointStamped div_pos;
+	PointStamped div_lio_pos, div_orb_pos;
 
-	div_pos.header.frame_id = "map";
+	div_lio_pos.header.frame_id = "map";
+	div_orb_pos.header.frame_id = "map";
 	
 
 	getLidarPose(topic2,&orb_lidar_pos);
@@ -58,21 +39,26 @@ void callback(const OdometryConstPtr &topic1, const PoseStampedConstPtr &topic2)
 	tf::Vector3 init_lio_pose,init_orb_pose,lio_pose,orb_pose,lio_relative_pose,orb_relative_pose;
 	tf::Vector3 previous_lio_pose;
 	lio_pose = tf::Vector3(topic1->pose.pose.position.x,topic1->pose.pose.position.y,0.0);
-	orb_pose = tf::Vector3(topic2->pose.position.x,topic2->pose.position.y,0.0); 
+	orb_pose = tf::Vector3(orb_lidar_pos.pose.position.x,orb_lidar_pos.pose.position.y,0.0); 
 	//orbのカメラ座標からlidarの座標を算出
 	output_pub.publish(orb_lidar_pos);
 	if(!flag){
-		div_pos.point.x = lio_pose.getX();
-		div_pos.point.y = lio_pose.getY();
-		div_pos.point.z = 0.0;
-		output_division.publish(div_pos);
+		div_lio_pos.point.x = lio_pose.getX();
+		div_lio_pos.point.y = lio_pose.getY();
+		div_lio_pos.point.z = 0.0;
+		div_orb_pos.point.x = orb_pose.getX();
+		div_orb_pos.point.y = orb_pose.getY();
+		div_orb_pos.point.z = 0.0;
+		output_lio_division.publish(div_lio_pos);
+		output_orb_division.publish(div_orb_pos);
 		init_lio_pose = lio_pose; 
 		init_orb_pose = orb_pose;
 		previous_lio_pose = lio_pose;
 		flag = true;
 	}
 	if(distance<MAX_EVALUATION_DISTANCE){
-		distance += lio_pose.distance2(previous_lio_pose);
+		if(cnt%5==0)distance += lio_pose.distance2(previous_lio_pose);
+		cnt++;
 		previous_lio_pose = lio_pose;
 		lio_relative_pose = lio_pose-init_lio_pose;
 		orb_relative_pose = orb_pose-init_orb_pose;
@@ -80,8 +66,9 @@ void callback(const OdometryConstPtr &topic1, const PoseStampedConstPtr &topic2)
 	}
 	else{
 
-		printf("error:%lf\n",error);
+		printf("%lf\n",error);
 		error=0.0;
+		cnt=0;
 		distance=0.0;
 		flag=false;
 	}
@@ -89,21 +76,45 @@ void callback(const OdometryConstPtr &topic1, const PoseStampedConstPtr &topic2)
 
 
 }
+*/
+struct pose_t {
+	uint64_t stamp;
+	geometry_msgs::Pose pose;
+};
+
 int main(int argc, char **argv){
 	ros::init(argc, argv, "evaluater");
 
 	ros::NodeHandle n;
+	std::vector<pose_t> ref_pose,target_pose;
+	pose_t tmp_pose;
 
-	output_pub = n.advertise<PoseStamped>("/autotuner/lidar_pose",10);
-	output_division = n.advertise<PointStamped>("/autotuner/division",10);
-	message_filters::Subscriber<Odometry> pose_ref(n,"/aft_mapped_to_init",1);
-	message_filters::Subscriber<PoseStamped> pose_target(n,"/orb_slam2_stereo/pose",1);
+	io::CSVReader<8> target_csv("test.csv");
+	target_csv.read_header(io::ignore_extra_column,
+	"field.header.stamp",
+	"field.pose.position.x",
+	"field.pose.position.y",
+	"field.pose.position.z",
+	"field.orientation.x",
+	"field.orientation.y",
+	"field.orientation.z",
+	"field.orientation.w"
+	);
 
-	typedef sync_policies::ApproximateTime<Odometry,PoseStamped> MySyncPolicy;
-
-	Synchronizer<MySyncPolicy> sync(MySyncPolicy(10),pose_ref,pose_target);
-	sync.registerCallback(boost::bind(&callback,_1,_2));
-
+	while(target_csv.read_row(
+		tmp_pose.stamp,
+		tmp_pose.pose.position.x,
+		tmp_pose.pose.position.y,
+		tmp_pose.pose.position.z,
+		tmp_pose.pose.orientation.x,
+		tmp_pose.pose.orientation.y,	
+		tmp_pose.pose.orientation.z,
+		tmp_pose.pose.orientation.w)
+		){
+		
+		ref_pose.push_back(tmp_pose);
+	}
+	printf("%zu\n",ref_pose.size());
 	ROS_INFO("start\n");
 
 	ros::spin();
---
+++ src/makeCSV.cc
@@ -0,0 +1,81 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <autotuner/save.h>
+
+std::vector<std::string>data;
+const int MAX_ROW= 11;
+
+void writeCSV(const char *filename){
+	int row=1;
+	std::ofstream file(filename);
+	
+	for(int i=0;i<data.size();i++){
+		file << data[i];
+		if(row==MAX_ROW){
+			file << '\n';
+			row = 0;
+		}
+		else file << ',';
+		
+		row++;
+	}
+	
+	file.close();
+}
+
+bool saveCSV(autotuner::save::Request &req,
+autotuner::save::Response &res){
+
+	writeCSV(req.filename.c_str());
+	res.success = true;
+	return true;
+
+}
+void callback(const geometry_msgs::PoseStampedConstPtr &topic){
+	ROS_INFO("call back\n");
+	char str[30];
+	sprintf(str,"%lu%09lu",topic->header.stamp.sec,topic->header.stamp.nsec);
+	
+	data.push_back(str);
+	data.push_back(std::to_string(topic->header.seq));
+	data.push_back(str);
+	data.push_back(topic->header.frame_id);
+	data.push_back(std::to_string(topic->pose.position.x));
+	data.push_back(std::to_string(topic->pose.position.y));
+	data.push_back(std::to_string(topic->pose.position.z));
+	data.push_back(std::to_string(topic->pose.orientation.x));
+	data.push_back(std::to_string(topic->pose.orientation.y));
+	data.push_back(std::to_string(topic->pose.orientation.z));
+	data.push_back(std::to_string(topic->pose.orientation.w));
+	
+}
+
+void insert_label(void){
+	data.push_back("%time");
+	data.push_back("field.header.seq");
+	data.push_back("field.header.stamp");
+	data.push_back("field.header.frame_id");
+	data.push_back("field.pose.position.x");
+	data.push_back("field.pose.position.y");
+	data.push_back("field.pose.position.z");
+	data.push_back("field.orientation.x");
+	data.push_back("field.orientation.y");
+	data.push_back("field.orientation.z");
+	data.push_back("field.orientation.w");
+}
+int main(int argc, char **argv){
+	ros::init(argc, argv, "evaluater");
+
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("/orb_slam2_stereo/lidar_pose",1000,callback);
+	insert_label();
+	ros::ServiceServer service = n.advertiseService("saveCSV",saveCSV);
+	ROS_INFO("start\n");
+
+	ros::spin();
+	return 0;
+}
