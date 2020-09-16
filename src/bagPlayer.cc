826c590942ebf551af62e8bbf41eb0f30bc43ce3
shohei morikawa <sh.oh54t@gmail.com>
2020-09-16 11:13:13 +0900

    rosbag 実装

--- src/bagPlayer.cc
+++ src/bagPlayer.cc
@@ -1,10 +1,28 @@
 #include <cstdlib>
 #include <string>
 #include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/String.h>
+
+ros::Publisher pub;
+
+void playRosbag(const std_msgs::String::ConstPtr& msg){
+	std::string cmd;
+	std::stringstream ss;
+	std_msgs::Empty done;
+	ss << "rosbag play " << msg->data << " -r 0.1"  ;
+	cmd = ss.str();
+	ROS_INFO("execute cmd\n");
+	system(cmd.c_str());
+	ROS_INFO("finish\n");
+	pub.publish(done);
+}
 int main(int argc,char **argv){
-	std::string filename;
 	ros::init(argc, argv, "bagplayer");
 	ros::NodeHandle n;
+	pub = n.advertise<std_msgs::Empty>("done", 1000);
+	ros::Subscriber sub = n.subscribe("play",1000,playRosbag);
+
 	ros::spin();
 	return 0;	
 }
